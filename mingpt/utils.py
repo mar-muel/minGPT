724a847b77b22109459127f9a6d5b7ba33da241e
Martin Müller <martin.mathias.mueller@gmail.com>
2021-04-13 22:01:53 +0200

    add top p logic

--- mingpt/utils.py
+++ mingpt/utils.py
@@ -16,8 +16,27 @@ def top_k_logits(logits, k):
     out[out < v[:, [-1]]] = -float('Inf')
     return out
 
+def top_p_logits(logits, top_p):
+    """Adapted from Huggingface/transformers"""
+    sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+    cumulative_probs = sorted_logits.softmax(dim=-1).cumsum(dim=-1)
+    sorted_indices_to_remove = cumulative_probs > top_p
+
+    # Remove tokens with cumulative top_p above the threshold (token with 0 are kept)
+    sorted_indices_to_remove = cumulative_probs > top_p
+
+    # Shift the indices to the right to keep also the first token above the threshold
+    sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+    sorted_indices_to_remove[..., 0] = 0
+
+    # scatter sorted tensors to original indexing
+    indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+    logits = logits.masked_fill(indices_to_remove, -float('Inf'))
+    return logits
+
+
 @torch.no_grad()
-def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None, top_p=None):
     """
     take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
     the sequence, feeding the predictions back into the model each time. Clearly the sampling
@@ -34,6 +53,8 @@ def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
         # optionally crop probabilities to only the top k options
         if top_k is not None:
             logits = top_k_logits(logits, top_k)
+        if top_p:
+            logits = top_p_logits(logits, top_p)
         # apply softmax to convert to probabilities
         probs = F.softmax(logits, dim=-1)
         # sample from the distribution or take the most likely
